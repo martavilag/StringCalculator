d5bc3b90b9f630aa202f8c4b3b3595d521129c04
mvila <mvila@voxelgroup.net>
2021-11-30 20:01:05 +0100

    feat: return a number when string is zero and the same number

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -8,7 +8,9 @@ namespace StringCalculatorKata
         {
             if (string.IsNullOrEmpty(numbers))
                 return 0;
-            return Int32.Parse(numbers);
+            if (!numbers.Contains(","))
+                return Int32.Parse(numbers);
+            return Int32.Parse(numbers.Substring(0,1));
         }
     }
 }
