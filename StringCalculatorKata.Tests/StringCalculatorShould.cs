3fce79b1084c0732c1a8bf1db6c2e69b5c8319b5
mvila <mvila@voxelgroup.net>
2021-11-30 19:47:28 +0100

    test: return a number when string is the same number

--- StringCalculatorKata.Tests/StringCalculatorShould.cs
+++ StringCalculatorKata.Tests/StringCalculatorShould.cs
@@ -11,5 +11,16 @@ namespace StringCalculatorKata.Tests
             var result = StringCalculator.Sum("");
             result.Should().Be(0);
         }
+
+        [Theory]
+        [InlineData("1",1)]
+        [InlineData("2",2)]
+        [InlineData("3",3)]
+        public void ReturnANumberWhenStringIsTheSameNumber(string number, int expected)
+        {
+            var result = StringCalculator.Sum(number);
+            result.Should().Be(expected);
+        }
+        
     }
 }
