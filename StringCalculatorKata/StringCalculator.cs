417925fa64b6fa5749ea352a1747c384f4e6dcc3
mvila <mvila@voxelgroup.net>
2021-11-30 19:50:39 +0100

    feat: return a number when string is the same number

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -1,10 +1,14 @@
-﻿namespace StringCalculatorKata
+﻿using System;
+
+namespace StringCalculatorKata
 {
     public class StringCalculator
     {
         public static int Sum(string numbers)
         {
-            return 0;
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+            return Int32.Parse(numbers);
         }
     }
 }
